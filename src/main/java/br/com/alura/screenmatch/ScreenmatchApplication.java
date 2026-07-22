ac4c3fbc46dc91a160fff9300eb17da6f1d694a1
WendellCamilo <wendell_cadeado@hotmail.com>
2026-07-21 21:26:19 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,18 @@
+package br.com.alura.screenmatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner { //O que é CommandLineRunner?
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Olá, Mundo!");
+	}
+}
