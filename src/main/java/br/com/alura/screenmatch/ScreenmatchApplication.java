0f7bd601f315c96d81830b28993c671576747716
WendellCamilo <wendell_cadeado@hotmail.com>
2026-07-23 20:36:05 -0300

    Adicionado classe para requisições API

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,5 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.service.ConsumoApi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,8 +13,6 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=Lost&apikey=b777f3d5");
-		System.out.println(json);
+		System.out.println("Olá, Mundo!");
 	}
 }
