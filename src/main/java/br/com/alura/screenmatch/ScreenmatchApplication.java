b68525a76c8aae2b6c64e37d1fcd9574ce37b2cb
WendellCamilo <wendell_cadeado@hotmail.com>
2026-07-23 07:14:05 -0300

    primeiro commit

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner { //O que é CommandLineRunner?
+public class ScreenmatchApplication implements CommandLineRunner { //CommandLineRunner é uma ‘interface’ do Spring que permite executar código assim que a aplicação iniciar!
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
