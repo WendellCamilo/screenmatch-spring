3b44fd7e24339026e0fb10d7751cb11440e26a74
WendellCamilo <wendell_cadeado@hotmail.com>
2026-07-23 07:24:22 -0300

    descrição do que foi feito

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
+public class ScreenmatchApplication implements CommandLineRunner { //Teste git
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
